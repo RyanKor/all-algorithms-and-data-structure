48ee0e1db9eb0d7e408c55f53ece63927cdedafa
RyanKor <equuse3144@gmail.com>
2020-09-17 08:43:26 +0700

    some error edited

--- optional_challenges/02_answers.js
+++ optional_challenges/02_answers.js
@@ -1,8 +1,7 @@
 function areThereDuplicates(...args) {
   // Two pointers
   // Pointer를 활용한 이 문제에 해답이 틀린 것으로 나오는데, 답을 수정할 필요가 있다
-  //   args.sort((a, b) => a > b);  //여기서 값이 틀리다고 나오는데,
-  args.sort(); //이렇게 바꿔서 문자열도 알아서 정렬되게끔 해줘야하는 이슈가 있는 것 같다.
+  args.sort((a, b) => a > b); //여기서 값이 틀리다고 나오는데,
   let start = 0;
   let next = 1;
   while (next < args.length) {
--- optional_challenges/02_three_duplicates.js
+++ optional_challenges/02_three_duplicates.js
@@ -2,7 +2,7 @@
 
 function areThereDuplicates(...args) {
   // Two pointers
-  args.sort((a, b) => a > b);
+  args.sort(); //이렇게 바꿔서 문자열도 알아서 정렬되게끔 해줘야하는 이슈가 있는 것 같다.
   let start = 0;
   let next = 1;
   while (next < args.length) {
