88a1a7e827d7206c0f1a931de489decd6022826e
ryanKor <equus3144@gmail.com>
2021-04-09 11:27:37 +0900

    sorting

---
+++ python/BOJ/sorting/10_x_sort.py
@@ -0,0 +1,30 @@
+'''
+문제
+수직선 위에 N개의 좌표 X1, X2, ..., XN이 있다. 이 좌표에 좌표 압축을 적용하려고 한다.
+
+Xi를 좌표 압축한 결과 X'i의 값은 Xi > Xj를 만족하는 서로 다른 좌표의 개수와 같아야 한다.
+
+X1, X2, ..., XN에 좌표 압축을 적용한 결과 X'1, X'2, ..., X'N를 출력해보자.
+
+입력
+첫째 줄에 N이 주어진다.
+
+둘째 줄에는 공백 한 칸으로 구분된 X1, X2, ..., XN이 주어진다.
+
+출력
+첫째 줄에 X'1, X'2, ..., X'N을 공백 한 칸으로 구분해서 출력한다.
+
+제한
+1 ≤ N ≤ 1,000,000
+-109 ≤ Xi ≤ 109
+'''
+import sys
+n = int(sys.stdin.readline())
+
+lst = list(map(int, sys.stdin.readline().split()))
+
+arr2 = list(sorted(set(lst)))
+dic = {value: index for index, value in enumerate(arr2)}
+
+for element in lst:
+    print(dic[element], end= " ")
