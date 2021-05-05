745bf6d4e3312dd83b8331d5d0df0fa8477f8bb8
ryanKor <equus3144@gmail.com>
2021-05-06 01:05:15 +0900

    kakao, programmers algorithms solved

--- kakao/2018_kakao_blind/dart_game.py
+++ kakao/2018_kakao_blind/dart_game.py
@@ -3,17 +3,17 @@ def solution(dartResult):
     score = []
     n = ''
     for i in dartResult:
-        if i.isnumeric():
+        if i.isnumeric(): # 테스트케이스가 숫자만 있거나 알파벳, 특수문자 만나기 전까지
             n += i
-        elif i == 'S':
+        elif i == 'S': # single -> 1 제곱
             score.append(int(n) ** 1)
-            n = ''
-        elif i == 'D':
+            n = '' # n 초기화
+        elif i == 'D': # double -> 2 제곱
             score.append(int(n) ** 2)
-            n = ''
-        elif i == 'T':
+            n = '' # n 초기화
+        elif i == 'T': # triple -> 3 제곱
             score.append(int(n) ** 3)
-            n = ''
+            n = '' # n 초기화
         elif i == '*':
             if len(score) > 1:
                 score[-2] *= 2
---
+++ kakao/2018_kakao_blind/secret_map.py
@@ -0,0 +1,15 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        result = bin(arr1[i] | arr2[i])[2:]
+        if len(result) < n:
+            result = '0'*(n-len(result))  + result
+        text = ''
+        for i in result:
+            if i == '1':
+                text += "#"
+            else:
+                text += " "
+        answer.append(text)
+    
+    return answer
